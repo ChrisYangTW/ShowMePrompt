4ec48f80d7285d10a5d5b5ba708cb60543698d79
ChrisYangTw <anti@hotmail.com.tw>
2023-04-24 17:45:16 +0800

    Change the scrollAreaWidgetContents_layout from QHBoxLayout to QGridLayout

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 from pathlib import Path
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel, QGridLayout
 from PySide6.QtGui import QPixmap, QDragEnterEvent, QDropEvent, QGuiApplication, QKeyEvent
 from PySide6.QtCore import Qt, Slot
 
@@ -16,17 +16,14 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # self.ui.scrollAreaWidgetContents add horizontal layout
-        self.scrollAreaWidgetContents_layout = QHBoxLayout()
+        # self.ui.scrollAreaWidgetContents add QGridLayout layout
+        self.scrollAreaWidgetContents_layout = QGridLayout()
         self.scrollAreaWidgetContents_layout.setObjectName(u"scrollAreaWidgetContents_layout")
         self.ui.scrollAreaWidgetContents.setLayout(self.scrollAreaWidgetContents_layout)
 
-        # Disable the open_with_default_button (as there are no image files to open initially)
-        self.ui.open_with_default_button.setEnabled(False)
-
         # Set up main_image_label(QLabel) drag and drop events
         # self.ui.main_image_label.mousePressEvent = lambda event: self.open_and_show_image()
-        # self.ui.main_image_label.mousePressEvent = lambda event: self.show_image_use_preview(event)
+        self.ui.main_image_label.mousePressEvent = lambda event: self.show_image_use_preview(event)
         self.ui.main_image_label.dragEnterEvent = lambda event: self.main_image_label_dragEnterEvent(event)
         self.ui.main_image_label.dropEvent = lambda event: self.main_image_label_dropEvent(event)
 
@@ -34,6 +31,8 @@ class MainWindow(QMainWindow):
         self.ui.open_file_button.clicked.connect(self.open_and_show_image)
         self.ui.open_with_default_button.clicked.connect(self.show_image_use_preview)
         self.ui.gallery_refresh_button.clicked.connect(lambda: self.gallery(self.open_folder_path_last))
+        # Disable the open_with_default_button (as there are no image files to open initially)
+        self.ui.open_with_default_button.setEnabled(False)
 
         # Set up two copy buttons and connect them with the corresponding functions
         self.ui.copy_without_settings_button.clicked.connect(self.copy_raw_without_settings)
@@ -44,24 +43,25 @@ class MainWindow(QMainWindow):
         self.default_app = None
         self.current_image_raw_without_settings = ''
         self.current_image_raw = ''
-        self.gallery_image_label_dict = {}
-        self._gallery_images_path = []
-        self._gallery_image_index_end = 1
-        self._gallery_image_index_pointer = 1
+        self._gallery_image_label_dict = {}
+        self._gallery_images_file_path = []
+        self._gallery_image_index_end = 0
+        self._gallery_image_index_pointer = 0
 
     def keyPressEvent(self, event: QKeyEvent):
         """
-        keyboard event, only works when self.gallery_image_label_dict is not empty
+        keyboard event(for A and S), A/S controls selecting different images.
+        only works when self._gallery_image_label_dict is not empty
         :param event:
         :return:
         """
-        if event.key() == Qt.Key_A and self.gallery_image_label_dict:
-            if self._gallery_image_index_pointer > 1:
-                file_path = self._gallery_images_path[self._gallery_images_path.index(self.file_path) - 1]
+        if event.key() == Qt.Key_A and self._gallery_image_label_dict:
+            if self._gallery_image_index_pointer > 0:
+                file_path = self._gallery_images_file_path[self._gallery_image_index_pointer - 1]
                 self.open_and_show_image(file_path)
-        elif event.key() == Qt.Key_S and self.gallery_image_label_dict:
+        elif event.key() == Qt.Key_S and self._gallery_image_label_dict:
             if self._gallery_image_index_pointer < self._gallery_image_index_end:
-                file_path = self._gallery_images_path[self._gallery_images_path.index(self.file_path) + 1]
+                file_path = self._gallery_images_file_path[self._gallery_image_index_pointer + 1]
                 self.open_and_show_image(file_path)
 
     def main_image_label_dragEnterEvent(self, event: QDragEnterEvent):
@@ -89,53 +89,54 @@ class MainWindow(QMainWindow):
 
     def open_and_show_image(self, file_path: Path = None) -> None:
         """
-        Open the file and display all its information, including updating the gallery.
+        Open the image and display, including updating the gallery and the index pointer
         :param file_path:
         :return:
         """
+        # If opened through the open button, there will be no file_path
         if not file_path:
             selected_file_path, _ = QFileDialog.getOpenFileName(self, "Open Image", str(self.open_folder_path_last),
                                                                 "Image Files (*.png *.jpg *.jpeg *.webp)")
             if not _:
-                # To prevent execution of the following code when no file is selected
                 return
             else:
                 self.file_path = Path(selected_file_path)
         else:
             self.file_path = file_path
+
         file_folder_path = self.file_path.parent
-        # Updates the gallery if the opened folder has been changed.
+        # Updating the gallery if the opened folder has been changed.
         if file_folder_path != self.open_folder_path_last:
             self.gallery(file_folder_path)
-            # for opening files from the last accessed directory
+            # For opening files from the last accessed directory
             self.open_folder_path_last = file_folder_path
-        # To read an image file and display it on the image_label (QLabel)
+
+        # To read an image file and display it, including showing prompts
         pixmap = QPixmap(self.file_path)
         self.ui.main_image_label.setPixmap(pixmap.scaled(self.ui.main_image_label.size(),
                                                          Qt.KeepAspectRatio,
                                                          Qt.SmoothTransformation))
+        self.show_info_to_text_browser(self.file_path)
 
+        # After successfully opening an image for the first time, enable the preview button
         if not self.ui.open_with_default_button.isEnabled() and sys.platform == 'darwin':
             self.ui.open_with_default_button.setEnabled(True)
 
-        self._gallery_image_index_pointer = self.gallery_image_label_dict[f'{self.file_path.name}'][2]
-
-        # To read the information of an image and display it in the corresponding QTextBrowser
-        self.show_info_to_text_browser(self.file_path)
+        # Updating self._gallery_image_index_pointer
+        self._gallery_image_index_pointer = self._gallery_images_file_path.index(self.file_path)
 
     @Slot()
     def show_image_use_preview(self, event=None):
         """
-        To open a file using the default program in Mac.
+        To open an image using the default program in Mac.
         """
         # todo: Currently, I don't have a Win system at hand to test.
         self.default_app = self.default_app or self.get_default_application()
         try:
             if not event:
                 subprocess.call(self.default_app + [str(self.file_path)])
-            elif event.button() == Qt.LeftButton:
-                # todo: for self.ui.main_image_label.mousePressEvent.
-                #       Need to implement the feature that the function won't trigger when the main_image_label is empty
+            elif event.button() == Qt.LeftButton and self.ui.main_image_label.pixmap():
+                # for self.ui.main_image_label.mousePressEvent
                 subprocess.call(self.default_app + [str(self.file_path)])
         except subprocess.CalledProcessError as e:
             print('Error:', e)
@@ -150,38 +151,38 @@ class MainWindow(QMainWindow):
         if not open_folder_path:
             return
 
-        # If there are widgets in the horizontal layout of gallery_display_layout,
-        # remove them first and clear self.gallery_image_label_dict.
+        # If there are widgets in the self.scrollAreaWidgetContents_layout(QGridLayout),
+        # remove them first and clear self._gallery_image_label_dict.
         try:
             self.clear_layout_widgets(self.scrollAreaWidgetContents_layout)
-            self.gallery_image_label_dict.clear()
+            self._gallery_image_label_dict.clear()
         except Exception as e:
             print(e)
 
-        self._gallery_images_path = sorted([file for file in open_folder_path.glob('*')
-                                           if file.suffix.lower() in ['.png', '.jpg', '.jpeg', '.webp']])
-        self._gallery_image_index_end = len(self._gallery_images_path)
+        self._gallery_images_file_path = sorted([file for file in open_folder_path.glob('*')
+                                                 if file.suffix.lower() in ['.png', '.jpg', '.jpeg', '.webp']])
+        self._gallery_image_index_end = len(self._gallery_images_file_path) - 1  # index is 0~
 
         # Use a QLabel to display each image in the scrollAreaWidgetContents_layout,
         # with each image having its own label and bound to a click event.
-        for i, image_path in enumerate(self._gallery_images_path):
+        for i, image_path in enumerate(self._gallery_images_file_path):
             pixmap = QPixmap(image_path)
             if pixmap.isNull():
                 continue
             label = QLabel()
             label.setPixmap(pixmap.scaledToHeight(95, Qt.SmoothTransformation))
             label.mousePressEvent = lambda event, path=image_path: self.open_and_show_image(path)
-            self.gallery_image_label_dict[f'{image_path.name}'] = [label, image_path, i+1]
-            self.scrollAreaWidgetContents_layout.addWidget(self.gallery_image_label_dict.get(f'{image_path.name}')[0])
+            self._gallery_image_label_dict[f'{image_path.name}'] = [label, image_path, i]
+            self.scrollAreaWidgetContents_layout.addWidget(self._gallery_image_label_dict.get(f'{image_path.name}')[0],
+                                                           0, i)
 
         # Update self._gallery_image_index_pointer for user clicked refresh button
         try:
-            self._gallery_image_index_pointer = self.gallery_image_label_dict[f'{self.file_path.name}'][2]
+            self._gallery_image_index_pointer = self._gallery_image_label_dict[f'{self.file_path.name}'][2]
         except KeyError as e:
             print(f'file {e} is deleted, move to first image')
-            first_file_path = next(iter(self.gallery_image_label_dict.values()))[1]
+            first_file_path = next(iter(self._gallery_image_label_dict.values()))[1]
             self.open_and_show_image(first_file_path)
-
 
     def copy_raw_without_settings(self):
         clipboard = QGuiApplication.clipboard()
